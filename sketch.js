618a46629adeb4b4a97783bcc65acf77ab9d47ba
Chloe Lee <38182677+chloelee0207@users.noreply.github.com>
2018-09-29 13:46:34 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,34 +1,82 @@
+let rowHeight;
+let xposSushi = [];
+let yposSushi = [];
+let imgSushi = [];
+let xdir = [];
+let sushiWidth;
+let sushiHeight;
+let numSushiPerRow = 7;
+let numRows = 12;
+let sushiSpeed = 3;
+
+function preload() {
+  sushi1 = loadImage('sushi1.png');
+  sushi2 = loadImage('sushi2.png')
+  sushiPictures = [sushi1,sushi2];
+}
+
 function setup() {
   // put setup code here
-  createCanvas(windowWidth,windowHeight);
-  background(0, 0, 0);
-  //ellipse (width/2,height/2,400,400);
+  createCanvas(windowWidth, windowHeight);
+  background(50);
+
+  rowHeight = windowHeight / 12;
+  sushiWidth = rowHeight*0.9;
+  sushiHeight = rowHeight * 0.5;
+
+  for (let j = 0; j < numRows; j++) {
+    for (let i = 0; i < numSushiPerRow; i++) {
+      xposSushi.push(i * width / numSushiPerRow + random(width/numSushiPerRow));
+      yposSushi.push(j * rowHeight + rowHeight/2 - sushiHeight/2);
+      imgSushi.push(random(sushiPictures));
+      if (j % 2 == 0) {
+        xdir.push(sushiSpeed);
+      } else {
+        xdir.push(-sushiSpeed);
+      }
+    }
+  }
 }
 
 function draw() {
-  // put drawing code here
+  background(50);
   
-  //fill(255, 0, 0);
-  //ellipse(width/2,height/2,200,200);
-  //fill(255, 255, 255);
-  //ellipse(280,280,200,200);  
+  for (let j = 0; j < numRows; j++) {
+    for (let i = 0; i < numSushiPerRow; i++) {
+      let index = (j * numSushiPerRow) + i;
+      xposSushi[index] = xposSushi[index] + xdir[index];
+      if (xposSushi[index] > width) {
+        xdir[index] = -xdir[index];
+        yposSushi[index] = yposSushi[index] + rowHeight;
+      } else if (xposSushi[index] < 0) {
+        xdir[index] = -xdir[index];
+        yposSushi[index] = yposSushi[index] + rowHeight;
+      }
+    
+      // Teleport
+      if (yposSushi[index] > numRows * rowHeight) {
+        yposSushi[index] = rowHeight/2 - sushiHeight/2;
+      }
+      image(imgSushi[index], xposSushi[index], yposSushi[index], sushiWidth, sushiHeight);
+    }
+  }
 
-  // fill(255, 0, 0);
-  // ellipse(100,100,100,100);
-  // fill(0, 255, 0);
-  // ellipse(250,250,100,100);
-  // fill(0, 0, 255);
-  // ellipse(400,400,100,100); 
-  fill(random(100,255),random(100,255),random(100,255));
-  let xpos = mouseX+random(-50,50);
-  let ypos = mouseY+random(-50,50);
-  let diameter = 10
-  // ellipse (xpos,ypos,diameter,diameter);
+  stroke(random(255), random(255), random(255));
+  line(0, rowHeight, windowWidth, rowHeight);
+  line(0, rowHeight * 2, windowWidth, rowHeight * 2);
+  line(0, rowHeight * 3, windowWidth, rowHeight * 3);
+  line(0, rowHeight * 4, windowWidth, rowHeight * 4);
+  line(0, rowHeight * 8, windowWidth, rowHeight * 8);
+  line(0, rowHeight * 9, windowWidth, rowHeight * 9);
+  line(0, rowHeight * 10, windowWidth, rowHeight * 10);
+  line(0, rowHeight * 11, windowWidth, rowHeight * 11);
+  // line(0, rowHeight*12, windowWidth, rowHeight*12);
+  fill(50);
+  rect(-10, 4 * rowHeight, width+10, 4 * rowHeight);
 
- 
-  fill(random(255),random(255),255);
-  textSize(36);
-  textAlign(CENTER);
+  fill(random(255), random(255), random(255));
+  textSize(72);
+  textAlign(CENTER, CENTER);
   textStyle(BOLD);
-  text('My Catsite',xpos,ypos);
+  text('Thank You, Wan Lin', windowWidth / 2, windowHeight / 2);
 }
